bb72fefebc1ac6a4b5ce1500ca78f4eefcaeb17c
Dmitry Eshman <89666814+bakko28@users.noreply.github.com>
2024-10-06 04:44:02 +0300

    add habit-page

--- app.js
+++ app.js
@@ -1,43 +1,51 @@
-const addNewHabbitBtn = document.querySelector('.add_new_habbit');
-addNewHabbitBtn.addEventListener('click', addNewHabbit);
 
-function closeForm() {
-    // Код для закрытия формы
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const addNewHabitBtn = document.querySelector('.add_new_habit');
+    addNewHabitBtn.addEventListener('click', addNewHabit);
 
-function addNewHabbit() {
+    const habitBtns = document.querySelectorAll('.habit')
+    habitBtns.forEach(button => (
+        button.addEventListener('click', openHabit)
+    ))
+});
+
+function addNewHabit() {
     console.log('start');
 
-    const formElement = document.querySelector('.add_new_habbit_form');
+    const formElement = document.querySelector('.add_new_habit_form');
     if (formElement) {
         console.log('Element found:', formElement);
         formElement.style.display = 'flex';
         console.log('Display set to flex');
 
-        // Добавляем обработчик клика только после отображения формы
         setTimeout(() => {
-            document.addEventListener('click', handleClickOutside);
-        }, 0); // 0 миллисекунд = следующий тик события
+            document.addEventListener('click', handleClickOutsideForm);
+        }, 0);
     } else {
         console.log('Element not found');
     }
 
-    const habbitName = document.querySelector('.add_new_habbit_form form .habbit-name').value;
-    const habbitCount = document.querySelector('.add_new_habbit_form form .habbit-count').value;
+    const habitName = document.querySelector('.add_new_habit_form form .habit-name').value;
+    const habitCount = document.querySelector('.add_new_habit_form form .habit-count').value;
 
-    document.querySelector('.done-habbit-btn').addEventListener('click', () => {
-        addNewHabbit(habbitName, habbitCount, id);
-        renderHabbit();
+    document.querySelector('.add_new_habit_form form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        let id = 467218;
+        addHabitOnDB(habitName, habitCount, id);
+        // renderHabit();
+
+        console.log('Форма отправлена без перехода на новую страницу');
     });
 }
 
-function handleClickOutside(event) {
-    const formElement = document.querySelector('.add_new_habbit_form');
+function handleClickOutsideForm(event) {
+    const formElement = document.querySelector('.add_new_habit_form');
     const displayValue = window.getComputedStyle(formElement).display;
     console.log('Display value:', displayValue);
 
     if (displayValue === 'flex') {
-        let form = document.getElementById('habbitForm');
+        let form = document.getElementById('habitForm');
         let isClickInside = form.contains(event.target);
 
         if (!isClickInside) {
@@ -45,11 +53,47 @@ function handleClickOutside(event) {
             console.log('Clicked outside, form hidden');
 
             // Удаляем обработчик клика после закрытия формы
-            document.removeEventListener('click', handleClickOutside);
+            document.removeEventListener('click', handleClickOutsideForm);
         }
     }
 }
 
-function renderHabbit() {
+function handleClickOutsidePage(event) {
+    const habitPage = document.querySelector('.habit_page');
+    const displayValue = window.getComputedStyle(habitPage).display;
+    console.log('Display value:', displayValue);
+
+    if (displayValue === 'flex') {
+        let page = document.getElementById('habitPage');
+        let isClickInside = page.contains(event.target);
+
+        if (!isClickInside) {
+            habitPage.style.display = 'none';
+            console.log('Clicked outside, page hidden');
+            document.removeEventListener('click', handleClickOutsidePage);
+        }
+    }
+}
+
+function openHabit() {
+    const habitPage = document.querySelector('.habit_page')
+    if (habitPage) {
+        console.log('Element found:', habitPage);
+        habitPage.style.display = 'flex';
+        console.log('Display set to flex');
+
+        setTimeout(() => {
+            document.addEventListener('click', handleClickOutsidePage);
+        }, 0);
+    } else {
+        console.log('Element not found');
+    }
+}
+
+function addHabitOnDB(habitName, habitCount, id) {
+    console.log('Name:', habitName, 'Habit Count:', habitCount, 'ID:', id);
+}
+
+function renderHabit() {
     // Код для рендеринга привычки
 }
